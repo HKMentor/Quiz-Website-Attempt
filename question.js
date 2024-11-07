27c6ca51e349dd21631b667e05a2fcffde4e2f14
HKMentor <fatimahooria717@gmail.com>
2024-11-07 20:55:12 +0500

    Quiz Website Attempt it

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What color are bananas when they are ripe?",
+        answer: "c. Yellow",
+        options: [
+            'a. Red',
+            'b. Green',
+            'c. Yellow',
+            'd. Blue'
+        ]
+    },
+    {
+        numb: 2,
+        question: "During which month do Muslims fast?",
+        answer: "b. Ramadan",
+        options: [
+            'a. Shawwal',
+            'b. Ramadan',
+            'c. Dhul-Hijjah',
+            'd. Safar'
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many pillars of Islam are there?",
+        answer: "b. Five",
+        options: [
+            'a. Three',
+            'b. Five',
+            'c. Seven',
+            'd. Ten'
+        ]
+    },
+    {
+        numb: 4,
+        question: "What do plants need to grow?",
+        answer: "d. Water",
+        options: [
+            'a. Rocks',
+            'b. Paper',
+            'c. Sand',
+            'd. Water'
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many legs does a spider have?",
+        answer: "c. Eight",
+        options: [
+            'a. Four',
+            'b. Six',
+            'c. Eight',
+            'd. Ten'
+        ]
+    },
+];
